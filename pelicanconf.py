6f4e351ebf9688b596447f2ab1b41072dc8b60c0
James Gadoury <gadouryjames@gmail.com>
2025-04-06 13:27:35 -0400

    Init

---
+++ pelicanconf.py
@@ -0,0 +1,20 @@
+AUTHOR = "James Gadoury"
+SITENAME = "James Gadoury Blog"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "America/New_York"
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = 10
+
+# Set a theme for your blog
+THEME = "pelican-themes/simple"
