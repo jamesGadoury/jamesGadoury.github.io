4d515f0e8e9103d418a9d80e548106864462ab78
James Gadoury <gadouryjames@gmail.com>
2025-04-06 13:39:04 -0400

    Add everything fuck it

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,13 +1,13 @@
-AUTHOR = "James Gadoury"
-SITENAME = "James Gadoury Blog"
-SITEURL = ""
+AUTHOR = "james gadoury"
+SITENAME = "blog"
+SITEURL = "https://jamesgadoury.github.io"
 
 PATH = "content"
 
 TIMEZONE = "America/New_York"
 DEFAULT_LANG = "en"
 
-# Feed generation is usually not desired when developing
+# TODO: do i want this?
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
@@ -16,5 +16,8 @@ AUTHOR_FEED_RSS = None
 
 DEFAULT_PAGINATION = 10
 
-# Set a theme for your blog
-THEME = "pelican-themes/simple"
+# TODO: find better theme
+# can use git clone https://github.com/getpelican/pelican-themes.git
+# THEME_PATH = 'pelican-themes'
+# THEME = 'simple'
+THEME = "notmyidea"
